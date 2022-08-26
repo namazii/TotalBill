3daa72ea6b939a0f6212bee7ab415761adb8e562
namazii <nazar383875@gmail.com>
2022-08-26 16:18:30 +0300

    fix(appdelegate): UIInterfaceOrientationMask change

--- TotalBill/Application/AppDelegate.swift
+++ TotalBill/Application/AppDelegate.swift
@@ -9,9 +9,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var orientationLock = UIInterfaceOrientationMask.portrait
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
+    }
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+            return self.orientationLock
     }
 }
 
